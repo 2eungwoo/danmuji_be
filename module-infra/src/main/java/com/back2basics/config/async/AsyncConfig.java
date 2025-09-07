34e9aa4955df3810be912eba026cf5f64bbbaf95
swlee <swo98@naver.com>
2025-09-07 18:10:23 +0900

    fix: 잘못된 retry 어노테이션 import구문 수정

--- module-infra/src/main/java/com/back2basics/config/async/AsyncConfig.java
+++ module-infra/src/main/java/com/back2basics/config/async/AsyncConfig.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableRetry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableRetry
