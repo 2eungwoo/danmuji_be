dea821e065933a204046f0adae95ff63f4eef7d8
swlee <swo98@naver.com>
2025-09-07 18:00:15 +0900

    feat: 비동기 처리 재시도를 위한 Spring Retry 설정

--- module-infra/src/main/java/com/back2basics/config/async/AsyncConfig.java
+++ module-infra/src/main/java/com/back2basics/config/async/AsyncConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableRetry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@EnableRetry
 @EnableAsync
 @Configuration
 @RequiredArgsConstructor
