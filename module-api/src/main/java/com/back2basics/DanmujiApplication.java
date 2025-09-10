28d9ea63d9402d5adc30131afd64a4f459cf7234
swlee <swo98@naver.com>
2025-09-10 16:39:26 +0900

    feat: 전역 캐시 활성화

--- module-api/src/main/java/com/back2basics/DanmujiApplication.java
+++ module-api/src/main/java/com/back2basics/DanmujiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.back2basics")
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = "com.back2basics")
 @EnableMongoRepositories(basePackages = "com.back2basics")
 @EnableScheduling
+@EnableCaching
 public class DanmujiApplication {
 
     public static void main(String[] args) {
